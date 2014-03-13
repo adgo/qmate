9bbe4c6e7b25e0405b08027e9f1b9923b1091cc6
adgo <adgo@mms-dresden.de>
2014-03-13 15:08:33 +0100

    fixed config of used diagram elements

--- MATE/org.tud.inf.st.mbt.ui/src/org/tud/inf/st/mbt/emf/ui/MBTConstants.java
+++ MATE/org.tud.inf.st.mbt.ui/src/org/tud/inf/st/mbt/emf/ui/MBTConstants.java
@@ -4,10 +4,11 @@ import org.eclipse.core.runtime.Platform;
 
 public interface MBTConstants {
 	public static final String PRODUCT_MOBILE = "org.tud.inf.st.mbt.ui.mobile";
+
 	public static final String PRODUCT_ADAPTIVE = "org.tud.inf.st.mbt.ui.adaptive";
 	
 	public static final boolean IS_MOBILE = Platform.getProduct().getId().equals(PRODUCT_MOBILE);
-	public static final boolean USE_VARIABILITY = !Platform.getProduct().getId().equals(PRODUCT_MOBILE);
+	public static final boolean USE_VARIABILITY = true;//!Platform.getProduct().getId().equals(PRODUCT_MOBILE);
 	public static final boolean USE_REALTIME = false && !Platform.getProduct().getId().equals(PRODUCT_MOBILE);
 
 			
